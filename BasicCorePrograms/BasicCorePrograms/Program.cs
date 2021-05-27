64972c05386cfb4bf9b4486ce1551998376d8851
VengalreddyAkhila <akhila.it1801@gmail.com>
2021-05-27 14:01:56 +0530

    Refactor:Primefactors

---
+++ BasicCorePrograms/BasicCorePrograms/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BasicCorePrograms
+{
+    class Program
+    {
+        static void Main(string[] arg)
+        {
+            Console.WriteLine("Welcome to basic core programs");
+
+            Console.WriteLine("Enter a choice : ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    FlipCoin flipCoin = new FlipCoin();
+                    flipCoin.CoinPercentage();
+                    break;
+                case 2:
+                    PrimeFactors primefactors = new PrimeFactors();
+                    primefactors.Factors();
+                    break;
+            }
+        }
+    }
+}
